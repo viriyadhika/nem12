447aaeee5da9ebe2e7d3714628358bfc3a2db2da
Viriyadhika <viriya.dhika0@gmail.com>
2024-06-15 01:10:20 +0800

    cli mode

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,8 +1,24 @@
-from .sql_generator import execute_sql, get_sql_from_nim12, generate_nmi_details
+from .utils.files import get_generated_mock_file_name, get_upload_path
+from .sql_generator import get_sql_from_nim12, generate_nmi_details
+import argparse
+import uuid
 
 if __name__ == "__main__":
-    # with open("app/files/res.txt") as file:
-    #     f = file.read()
-    #     execute_sql.run(f.split("\n"))
-    # get_sql_from_nim12()
-    generate_nmi_details(5000, "app/files/out.csv")
+    parser = argparse.ArgumentParser(
+        description="Run task", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("command", help='Either "perf_test", ""')
+    argument = vars(parser.parse_args())
+
+    print(f"Running command with {argument}")
+
+    try:
+        command = argument["command"]
+        if command == "perf_test":
+            task_id = str(uuid.uuid4())
+            generate_nmi_details(
+                10000, get_upload_path(get_generated_mock_file_name(task_id))
+            )
+            get_sql_from_nim12(task_id)
+    except KeyboardInterrupt:
+        pass
